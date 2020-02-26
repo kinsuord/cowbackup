2e78d4211e8819d68dcf2c5ee6e9079aaa42b138
Jorden Liang <kinsuord@gmail.com>
2020-02-26 16:46:32 +0800

    fix sync fault

--- cowbackup.py
+++ cowbackup.py
@@ -6,9 +6,7 @@ import yaml
 import shutil
 import time
 import datetime
-from distutils.util import strtobool
-
-update_time_file = 'last_update.txt'
+import pickle
 
 def match_filter(path, filter):
     if filter == None:
@@ -24,19 +22,9 @@ def match_filter(path, filter):
             return True
     return False
 
-def get_file_list(dir, filter):
-    file_dict = dict()
-    
-    for root, dirs, files in os.walk(dir):
-        for file in files:
-            path = os.path.join(root, file)
-            if match_filter(path, filter):
-                file_dict[path] = True
-
-    return file_dict
 
 class Op():
-    def __init__(self, method, target, source=None):
+    def __init__(self, target, method, source=None):
         self.method = method
         self.target = target
         self.source = source
@@ -53,17 +41,28 @@ class Op():
         elif self.method == 'delete':
             os.remove(self.target)
 
+def get_file_set(dir, filter):
+    file_set = set()
+    
+    for root, dirs, files in os.walk(dir):
+        for file in files:
+            path = os.path.join(root, file)
+            if match_filter(path, filter):
+                file_set.add(path)
+
+    return file_set
 
 if __name__ == '__main__':
-    # args
+    
+    #%% set argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("task_file", help="task setting in .yaml")
     parser.add_argument('--test', action='store_true', help="log out files but don't modify")
     args = parser.parse_args()
-
-    # tasks, filters
+    
+    #%% read tasks, filter
     with open(args.task_file, 'r') as f:
-         cfg = yaml.load(f, yaml.BaseLoader)
+        cfg = yaml.load(f, yaml.SafeLoader)
     tasks = cfg['tasks']
     filters = dict()
     for filter in cfg['filters']:
@@ -74,25 +73,32 @@ if __name__ == '__main__':
             else:
                 filters[filter['name']][rule] = filter[rule]
 
-    if os.path.isfile(update_time_file):
-        with open(update_time_file, 'r') as f:
-            last_update_time = float(f.read())
-    else:        
-        last_update_time = 0.0
-
-    # log file
+    #%% set logfile
     if args.test:
         logfile = 'test.log'
     else:
-        logfile = os.path.join('./log', datetime.datetime.now().strftime('%Y%m%d_%H%M%S.log'))
-    open(logfile, "w").close()
+        logfile = os.path.join('log', datetime.datetime.now().strftime('%Y%m%d_%H%M%S.log'))
+        if not os.path.isdir('log'):
+            os.makedirs('log')
+    # clear the log file
+    open(logfile, "w").close()                
+
+    #%% load record
+    if os.path.isfile('record.pkl'):
+        with open('record.pkl', 'rb') as f:
+            record = pickle.load(f)
+    else:
+        record = dict()
+        record['update_time'] = 0
+    
+    for task in tasks:
+        if not task['name'] in record:
+            record[task['name']] = set()
 
     for task in tasks:
         print('Backuping task {} ...'.format(task['name']))
 
-        task['dont_delete'] = strtobool(task['dont_delete'])
-
-        # making filter_list
+        #%% making filter_list
         for rule in ['include', 'exclude', 'must_include']:
             if not rule in task['filter']:
                 task['filter'][rule] = []
@@ -100,45 +106,46 @@ if __name__ == '__main__':
                 for defined_fliter in task['filter']['pre_defined']:
                     task['filter'][rule] = task['filter'][rule] + filters[defined_fliter][rule]
 
-        source_files = get_file_list(task['source'], task['filter'])
-        target_files = get_file_list(task['target'], None)
+        source_files = get_file_set(task['source'], task['filter'])
+        target_files = get_file_set(task['target'], task['filter'])
         print('Source files: {} Target files: {}'.format(len(source_files), len(target_files)))
         
         # List(Operation)
         ops = []
         
-        # update file
+        def to_source(target):
+            return target.replace(task['target'], task['source'])
+        
+        def to_target(source):
+            return source.replace(task['source'], task['target'])
+
+        #%% update file
         for source in source_files:
-            target = source.replace(task['source'], task['target'])
+            target = to_target(source)
             if target in target_files:
-
                 source_mtime = os.path.getmtime(source)
                 target_mtime = os.path.getmtime(target)
 
-                # source_update_time, target_update_time, last_update_time -> update ab , update ba , do nothing
-                if source_mtime > target_mtime and source_mtime > last_update_time:
-                    ops.append(Op('update', target, source))
-                elif target_mtime > source_mtime and target_mtime > last_update_time:
-                    ops.append(Op('update', source, target))
-            # if source have but target don't have
-            else:
-                source_mtime = os.path.getmtime(source)
-                if source_mtime > last_update_time:
-                    ops.append(Op('new', target, source))
-                elif not task['dont_delete']:
-                    ops.append(Op('delete', source))
+                if source_mtime > target_mtime and source_mtime > record['update_time']:
+                    ops.append(Op( target, 'update', source))
+                elif target_mtime > source_mtime and target_mtime > record['update_time']:
+                    ops.append(Op( source, 'update', target))
+        
+        #%% set previous file set
+        previous_target_files = record[task['name']]
+        previous_source_files = set()
+        for target in previous_target_files:
+            previous_source_files.add(to_source(target))
+        
+        for new_file in source_files - previous_source_files:
+            ops.append(Op( to_target(new_file), 'new', new_file))
+        for less_file in previous_source_files - source_files:
+            ops.append(Op( to_target(less_file), 'delete'))
 
-
-        # if target have but source don't have
-        for target in target_files:
-            source = target.replace(task['target'], task['source'])
-            if not source in source_files:
-                target_mtime = os.path.getmtime(target)
-                
-                if target_mtime > last_update_time:
-                    ops.append(Op('new', source, target))
-                elif not task['dont_delete']:
-                    ops.append(Op('delete', target))
+        for new_file in target_files - previous_target_files:
+            ops.append(Op( to_source(new_file), 'new', new_file))
+        for less_file in previous_target_files - target_files:
+            ops.append(Op( to_source(less_file), 'delete'))
 
         if not args.test:
             print('Copying and deleting task{}...'.format(task['name']))
@@ -153,7 +160,11 @@ if __name__ == '__main__':
                 f.write('{}\n'.format(op.__str__()))
             print('Write log file in ** {} **'.format(logfile))
 
-    # update lastupdate time
+        # update record
+        record[task['name']] = get_file_set(task['target'], task['filter'])
+    
+    record['update_time'] = time.time() + 3
+    
     if not args.test:
-        with open(update_time_file, 'w') as f:
-            f.write(str(time.time() + 5))
+        with open('record.pkl', 'wb') as f:
+            pickle.dump(record, f)
